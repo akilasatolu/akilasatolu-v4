c02f857d591df029fae672b3bce19a35ad6f4e8a
akilasatolu <akilasatolu@gmail.com>
2026-04-10 18:29:38 +0900

    update iconリストUI調整

--- src/organisms/IconList.tsx
+++ src/organisms/IconList.tsx
@@ -9,7 +9,7 @@ export const IconList = (props: IconListProps) =>{
   const childrenArray = React.Children.toArray(props.children);
   return (
     childrenArray.length > 0 && 
-    <ul className={`flex items-center gap-3 ${props.style}`}>
+    <ul className={`flex flex-wrap items-center gap-3 ${props.style}`}>
       {childrenArray.map((child, index) => (
         <li key={index}>
           {child}
