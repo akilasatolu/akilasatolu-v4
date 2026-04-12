f316690018fb3182cc5d6d06fbb8db8bc6672f36
akilasatolu <akilasatolu@gmail.com>
2026-04-12 20:39:44 +0900

    update profile img import 修正

--- src/molecules/Avatar.tsx
+++ src/molecules/Avatar.tsx
@@ -1,8 +1,10 @@
+import profile from "../assets/images/profile.webp";
+
 export const Avatar = () => {
   return (
     <div className="flex justify-center mb-2">
         <img
-            src="../src/assets/images/profile.webp"
+            src={profile}
             alt="profile"
             className="w-24 h-24 rounded-full object-cover p-1.5 neu"
         />
