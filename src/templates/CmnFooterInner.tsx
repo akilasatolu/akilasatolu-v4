7f21cd70e91a94a29bde93d3c07d43974838273a
akilasatolu <akila.satolu@gmail.com>
2026-04-14 22:27:36 +0900

    update margin調整

--- src/templates/CmnFooterInner.tsx
+++ src/templates/CmnFooterInner.tsx
@@ -7,7 +7,7 @@ type CmnFooterInnerProps = {
 export const CmnFooterInner = (props: CmnFooterInnerProps) =>{
   return (
     <footer className="w-full pl-4 pr-4">
-        <div className="max-w-xl mb-6 mt-6 mx-auto flex justify-center">
+        <div className="max-w-xl mb-6 mt-16 mx-auto flex justify-center">
             { props.children }
         </div>
     </footer>
